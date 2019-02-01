8200125cc4e5e76c53c6db15eaf9514c50d874c4
Alex Scarlett <alex.scarlett91@gmail.com>
2019-02-01 14:16:04 -0500

    Fixed 'create_table' query, API route of '/register' now uses UserRegister class and SQLite to register a user

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 
 # Config app
@@ -75,6 +76,7 @@ class ItemList(Resource):
 # Add resources to API
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
--- app/create_tables.py
+++ app/create_tables.py
@@ -4,7 +4,7 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-create_table = "INSERT TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
 connection.commit()
